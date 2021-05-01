21a5dd1a367c73efd8419eb68c52f8d7cb4796c5
Przemysław Ambroży <przemek19986@gmail.com>
2021-05-01 12:44:51 +0200

    Initial Commit

---
+++ Item Inventory/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Item Inventory
+//
+//  Created by Przemek Ambroży on 01/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
